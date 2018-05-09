5066afb81885048bc3557f1f8795fa09ad2cb487
Shane Scott <shaneradio@gmail.com>
2018-05-08 21:00:58 -0400

    front-to-back communication proven

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import withAuthorization from './Session/withAuthorization';
 import { db } from '../firebase/index';
+import axios from "axios";
 
 class HomePage extends Component {
   constructor(props) {

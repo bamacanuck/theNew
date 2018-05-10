090525c363c66885c11a702ed0b297ea992a1012
Candace Joyner <candace.joyner>
2018-05-10 19:04:37 -0400

    custom sytle for button

--- client/src/components/signout.js
+++ client/src/components/signout.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import { auth } from '../firebase/index';
+import { bootstrapUtils } from 'react-bootstrap/lib/utils';
+
+bootstrapUtils.addStyle(Button, 'custom');
 
 const SignOutButton = () =>
-  <Button color="link"
-    onClick={auth.doSignOut}
-  >
-    Sign Out
-  </Button>
+	<div>
+	  <style type="text/css">
+	  	{`
+	    .btn-custom {
+	        margin-top: 8pt;
+	        background-color: purple;
+	        color: white;
+	    }
+	    `}
+	  </style>
+	  <Button color="link" bsStyle="custom"
+	    onClick={auth.doSignOut}
+	  >	
+	    Sign Out
+	  </Button>
+  </div>
 
 export default SignOutButton;
